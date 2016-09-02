7be52ac1ad84bdaf95408e0376e9ed651fe59143
THUYD6 <THUYD6@THUY>
2016-09-02 20:15:06 +0700

    D

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    /// <summary>
+    /// Maxlength gioi han kich thuoc cua chuoi hoac so
+    /// </summary>
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+        public bool Status { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Footer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    /// <summary>
+    /// Name :Table:gen code ten table chinh la ten trong ngoac nhon
+    /// Key ghi chu truong nay la khoa chinh
+    /// required :ghi chú trường này là không thể bỏ trống
+    /// </summary>
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
