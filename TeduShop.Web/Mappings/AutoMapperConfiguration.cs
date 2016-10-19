afdcbc28b3f60f8e429527382aee08a40fffc71a
THUYD6 <THUYD6@THUY>
2016-10-19 22:28:48 +0700

    test loi bai 23 chua chay duoc

--- TeduShop.Web/Mappings/AutoMapperConfiguration.cs
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -12,7 +12,9 @@ namespace TeduShop.Web.Mappings
             Mapper.CreateMap<Post, PostViewModel>();
             Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
             Mapper.CreateMap<Tag, TagViewModel>();
-            Mapper.CreateMap<PostTag, PostTagViewModel>(); 
+            Mapper.CreateMap<PostTag, PostTagViewModel>();
+            Mapper.CreateMap<Product, ProductViewModel>();
+            Mapper.CreateMap<ProductTag, ProductTagViewModel>();
         }
     }
 }
